03946bd562eeaa1023fc91fe5ebc81482e6362ae
Jonathan Kim <jkimbo@gmail.com>
2012-01-07 01:44:34 +0000

    Auto updating mail to link

--- js/script.js
+++ js/script.js
@@ -3,14 +3,15 @@
  */
 $(document).ready(function() {
     var name;
-    var email = $('#emailcontent').html();
+    var email = {};
+    email.body = $('#emailcontent').html();
+    email.to = 'example@imperial.ac.uk';
+    email.subject = 'Holland Park';
 
     $('header h1').fitText(1.2, { minFontSize: '30px' });
 
     var mailLink = $('#mailLink');
     mailLink.fitText(0.6, {}, function() {
-        console.log(mailLink.height());
-        //$('#name').height(mailLink.height());
     });
     $('#name').fitText(0.8);
 
@@ -25,16 +26,32 @@ $(document).ready(function() {
         return false;
     });
 
+    $('#text textarea').keyup(function() {
+        email.body = $('#text textarea').val();
+        setMailTo();
+    });
+
     function showTemplate(name) {
         if(!name) name = 'YOURNAME';
-        email += name;
-        $('#template textarea').val(email);
+        email.body += name;
+        setTemplate();
+        setMailTo();
         $('#setName').fadeOut('500', function() {
             $('#template').fadeIn('1000');
             $('html, body').animate({
                     scrollTop: $("#template").offset().top
             }, 1000);
         });
+    }
+
+    function setTemplate() {
+        $('#template textarea').val(email.body);
+        $('#subjectcont #subject').val(email.subject);
+        $('#sendto #emailto').val(email.to);
+    }
+
+    function setMailTo() {
+        $('#mailto').attr('href', 'mailto:'+email.to+'?subject='+email.subject+'&body='+escape(email.body));
     }
 });
 
