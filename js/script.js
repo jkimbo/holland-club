4c2e5ae039a773a5c2f454e0512d040c26d33a32
Jonathan Kim <jkimbo@gmail.com>
2012-01-07 01:06:57 +0000

    Takes in user name and produces template email

--- js/script.js
+++ js/script.js
@@ -2,7 +2,39 @@
  * Author: J.Kim
  */
 $(document).ready(function() {
+    var name;
+    var email = $('#emailcontent').html();
+
     $('header h1').fitText(1.2, { minFontSize: '30px' });
-    $('#mailLink').fitText(0.6);
+
+    var mailLink = $('#mailLink');
+    mailLink.fitText(0.6, {}, function() {
+        console.log(mailLink.height());
+        //$('#name').height(mailLink.height());
+    });
+    $('#name').fitText(0.8);
+
+    mailLink.click(function() {
+        name = $('#name').val();
+        showTemplate(name);
+        return false;
+    });
+
+    $('#showTemplate').click(function() {
+        showTemplate();
+        return false;
+    });
+
+    function showTemplate(name) {
+        if(!name) name = 'YOURNAME';
+        email += name;
+        $('#template textarea').val(email);
+        $('#setName').fadeOut('500', function() {
+            $('#template').fadeIn('1000');
+            $('html, body').animate({
+                    scrollTop: $("#template").offset().top
+            }, 1000);
+        });
+    }
 });
 
